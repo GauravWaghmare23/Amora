cb68d2a6b0cd8d027716cc2ca7ea7a75b64ce2cd
gauravwaghmare23 <gauravwaghmare95032@gmail.com>
2025-11-01 22:07:53 +0530

    .

--- App.js
+++ App.js
@@ -1,12 +1,10 @@
-import { StatusBar, StyleSheet, Text, useColorScheme, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
 
   return (
     <SafeAreaView style={stylesheet.flex1}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       <View style={stylesheet.container}>
         <Text>Hinge Dating app</Text>
       </View>
