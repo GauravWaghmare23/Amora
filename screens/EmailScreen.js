946688c70fdc817b247b2e4f9f2b402bed5e7e10
gauravwaghmare23 <gauravwaghmare95032@gmail.com>
2025-11-02 17:07:53 +0530

    EmailScreen created

--- screens/EmailScreen.js
+++ screens/EmailScreen.js
@@ -1,14 +1,90 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+/* eslint-disable react-native/no-inline-styles */
+import { Image, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import FontAwesome6Icon from 'react-native-vector-icons/FontAwesome6';
+import { useNavigation } from '@react-navigation/native';
 
 const EmailScreen = () => {
+  const [email,setEmail] = useState('');
+  const navigation = useNavigation();
+  const handleNext = () => {
+    navigation.navigate('Password');
+  }
   return (
-    <View>
-      <Text>EmailScreen</Text>
-    </View>
-  )
-}
+    <SafeAreaView style={{ flex: 1, backgroundColor: '#f7fae1' }}>
+      <View style={{ marginTop: 100, marginHorizontal: 20 }}>
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+          }}
+        >
+          <View
+            style={{
+              width: 44,
+              height: 44,
+              borderRadius: 22,
+              borderWidth: 2,
+              backgroundColor: '#f7fae1',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          >
+            <FontAwesome6Icon name="envelope" size={26} color="black" />
+          </View>
+          <Image
+            style={{ width: 100, height: 40 }}
+            source={{
+              uri: 'https://cdn-icons-png.flaticon.com/128/10613/10613685.png',
+            }}
+          />
+        </View>
+        <View style={{ marginTop: 30 }}>
+          <Text
+            style={{
+              fontSize: 25,
+              fontWeight: 'bold',
+              fontFamily: 'GezzaPro-Bold',
+            }}
+          >
+            What's your Email?
+          </Text>
+          <TextInput
+            value={email}
+            onChangeText={text => {
+              setEmail(text);
+            }}
+            placeholder="Email (required)"
+            placeholderTextColor={'#BEBEBE'}
+            style={{
+              width: 340,
+              marginVertical: 10,
+              marginTop: 25,
+              borderBottomColor: 'black',
+              borderBottomWidth: 1,
+            }}
+          />
+        </View>
+        <TouchableOpacity
+          onPress={handleNext}
+          style={{
+            marginTop: 30,
+            marginLeft: 'auto',
+            width: 44,
+            height: 44,
+            borderRadius: 22,
+            borderWidth: 3,
+            backgroundColor: '#f7fae1',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          <FontAwesome6Icon name="arrow-right" size={35} color="black" />
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+};
 
-export default EmailScreen
-
-const styles = StyleSheet.create({})
+export default EmailScreen;
